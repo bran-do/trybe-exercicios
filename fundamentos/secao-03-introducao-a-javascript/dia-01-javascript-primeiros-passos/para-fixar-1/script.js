4c1709549dcfd7eb8903208e350ad1026e5b1a7b
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-05-01 16:09:44 -0300

    Conclusão dos 6 exercícios 'Para Fixar' 1-3.1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar-1/script.js
@@ -0,0 +1,14 @@
+/*
+1. Crie uma variável chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+2. Crie uma variável chamada birthCity e atribua a ela a sua cidade natal.
+3. Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+4. Utilize o console.log() para imprimir as variáveis que você criou.
+*/
+
+let myName = "Felipe";
+let birthCity = "Porto Alegre";
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
