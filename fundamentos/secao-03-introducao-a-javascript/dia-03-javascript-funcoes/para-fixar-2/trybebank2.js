25a8b426aa1967ffc1f832cf05d85e3fcb07a1a5
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-05-04 00:17:28 -0300

    Conclusão do exercício da categoria 'Parâmetros de funções' ou 'Para Fixar 2' 1-3.3

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/para-fixar-2/trybebank2.js
@@ -0,0 +1,45 @@
+// Incluir clientes no TrybeBank
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function incluiCliente(nome) {
+    if (typeof(nome) === 'string') {
+        clientesTrybeBank.push(nome);
+        return '"' + nome + '" foi adicionado à lista de clientes.';
+    }
+    else {
+        return 'O nome precisa ser um string.';
+    }
+}
+
+// Testes de inclusão de cliente
+console.log(incluiCliente('Felipe'));
+console.log(clientesTrybeBank);
+console.log("");
+console.log(incluiCliente(6));
+console.log("");
+
+// Excluir clientes no TrybeBank
+
+function excluiCliente(nome) {
+    if (typeof(nome) === 'string') {
+        if (clientesTrybeBank.includes(nome)) {
+            let indexNome = clientesTrybeBank.indexOf(nome);
+            clientesTrybeBank.splice(indexNome, 1);
+            return '"' + nome + '" foi excluído da lista de clientes.';
+        }
+        else {
+            return '"' + nome + '" não foi localizado na lista de clientes.'
+        }
+    }
+    else {
+        return 'O nome precisa ser um string.'
+    }
+}
+
+// Testes exclusão de cliente
+console.log(excluiCliente('Gus'));
+console.log(clientesTrybeBank);
+console.log("");
+console.log(incluiCliente(6));
+console.log("");
