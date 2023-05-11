c5def4ce560b908546d093a139914397d97c1df1
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-05-11 01:34:17 -0300

    Exercício de prática de seletores

---
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicio-seletores/script.js
@@ -0,0 +1,5 @@
+let header = document.getElementById("header-container");
+header.innerText = "ADMINISTRADOR DE TEMPO DA TRYBE"
+
+let emergency = document.querySelector(".emergency-tasks");
+let nonEmergency = docmeunt.querySelectorAll(".no-emergency-tasks");
