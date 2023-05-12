54e7f4e2faa9f1867574a43351a8b92212fd2a55
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-05-11 23:38:33 -0300

    Conclusão do exercício 'criando-elementos' 1.4-2

---
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/criando-elementos/script.js
@@ -0,0 +1,18 @@
+// Crie um irmão para elementoOndeVoceEsta.
+const pai = document.querySelector('#pai');
+let irmao = document.createElement('section');
+pai.appendChild(irmao);
+
+// Crie um filho para elementoOndeVoceEsta.
+const ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let filho1 = document.createElement('section');
+ondeVoceEsta.appendChild(filho1);
+
+// Crie um filho para primeiroFilhoDoFilho.
+primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let filho2 = document.createElement('section');
+primeiroFilhoDoFilho.append(filho2);
+
+// A partir desse filho criado, acesse terceiroFilho.
+filhoCriado = document.querySelector('#primeiroFilhoDoFilho').firstChild;
+terceiroFilho = document.querySelector(filhoCriado).parentElement.parentElement.nextElementSibling;
