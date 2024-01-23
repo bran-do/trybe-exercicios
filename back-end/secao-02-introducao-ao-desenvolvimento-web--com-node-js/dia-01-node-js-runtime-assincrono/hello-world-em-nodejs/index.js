89651d55d4a4db4ce81a50002edb783320408ec2
felipe <brandaofelipebrandao@gmail.com>
2024-01-23 20:49:36 -0300

    Atualização da main

--- back-end/secao-02-introducao-ao-desenvolvimento-web--com-node-js/dia-01-node-js-runtime-assincrono/hello-world-em-nodejs/index.js
+++
@@ -1,6 +0,0 @@
-const readline = require('readline-sync');
-
-const nome = readline.question('Qual é o seu nome? ');
-const idade = readline.questionInt('Qual é a sua idade? ');
-
-console.log(`Olá, ${nome}! Parece que você tem ${idade} anos...`);
