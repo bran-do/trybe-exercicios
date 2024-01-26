749fb80fdb597d76ae758fe0ac74a05134997984
felipe <brandaofelipebrandao@gmail.com>
2024-01-26 17:06:08 -0300

    Exercício 1: retornar a quantidade total de chocolates 3-3.3

--- back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-03-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-03-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -87,7 +87,7 @@ describe('Testando a API Cacau Trybe', () => {
           name: 'Mounds',
           brandId: 3,
         },
-);
+      );
     });
   });
 
@@ -123,4 +123,15 @@ describe('Testando a API Cacau Trybe', () => {
       ]);
     });
   });
+
+  describe('Usando o método GET em /chocolates/total', function() {
+    it('Retorna a quantidade total de chocolates cadastrados', async function() {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal({ "totalChocolates": 4 });
+    })
+  })
 });
