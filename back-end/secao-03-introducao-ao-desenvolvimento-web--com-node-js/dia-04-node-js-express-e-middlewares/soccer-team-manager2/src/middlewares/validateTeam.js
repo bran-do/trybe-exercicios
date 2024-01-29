603af83acee205820e32390f407daf19ff967b07
felipe <brandaofelipebrandao@gmail.com>
2024-01-29 01:47:36 -0300

    Mensagens de erro personalizadas na validação do time; API Soccer Manager 3-3.4

--- back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-04-node-js-express-e-middlewares/soccer-team-manager2/src/middlewares/validateTeam.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-04-node-js-express-e-middlewares/soccer-team-manager2/src/middlewares/validateTeam.js
@@ -1,10 +1,11 @@
 // Adicionando um middleware para verificar se o formato do time passado via requisição é válido
 const validateTeam = (req, res, next) => {
-  const requiredProps = ['id', 'name', 'initials'];
+  const { name, initials } = req.body;
 
-  (requiredProps.every((prop) => prop in req.body))
-    ? next()
-    : res.sendStatus(400);
-}
+  if (!name) { return res.status(400).json({ message: "Property 'name' is mandatory." }) };
+  if (!initials) { return res.status(400).json({ message: "Property 'initials' is mandatory" }) };
+
+  next();
+}  
 
 module.exports = validateTeam;
