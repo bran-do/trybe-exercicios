9e4778b89d6d4701956ed08a414c3acecc9b8b59
felipe <brandaofelipebrandao@gmail.com>
2024-01-30 18:14:34 -0300

    POST /people, conexão MySQL + Express, query INSERT com base na requisição

--- back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-05-node-js-express-e-mysql/trybecash-api/src/app.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web--com-node-js/dia-05-node-js-express-e-mysql/trybecash-api/src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
+const peopleRoutes = require('./routes/peopleRoutes');
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/people', peopleRoutes);
+
+app.get('/people', (_req, res) => {
+  res.status(200).json({ message: "hi"})
+});
 
 module.exports = app;
