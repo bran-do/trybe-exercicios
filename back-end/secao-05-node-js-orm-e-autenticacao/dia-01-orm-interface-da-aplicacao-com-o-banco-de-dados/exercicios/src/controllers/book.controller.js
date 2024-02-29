578de8fb6c1f5ecfae232441e054a38c83038b4f
felipe <brandaofelipebrandao@gmail.com>
2024-02-29 17:07:20 -0300

    Exercicios do 1 ao 8 -- 2-5.1

---
+++ back-end/secao-05-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/controllers/book.controller.js
@@ -0,0 +1,20 @@
+const bookService = require('../services/book.service');
+
+const getAll = async (_req, res) => {
+    const books = await bookService.getAll();
+    return res.status(200).json(books);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const book = await bookService.getById(id);
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+  
+  return res.status(200).json(book);
+};
+
+module.exports = {
+  getAll,
+  getById,
+}
--- back-end/secao-05-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/index.js
+++ back-end/secao-05-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bookController = require('./controllers/book.controller');
 
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
+
+app.get('/books', bookController.getAll);
+app.get('/books/:id', bookController.getById);
