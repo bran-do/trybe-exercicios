2dedf8939abdbee08a341a5975c0134e9af9ea4c
felipe <brandaofelipebrandao@gmail.com>
2024-04-01 19:21:18 -0300

    Exercicio 4 - 3-8.2

---
+++ back-end/secao-08-poo-programacao-orientada-a-objetos-e-solid/dia-02-heranca-e-composicao/exercicio-pratico/classes/Subject.ts
@@ -0,0 +1,11 @@
+export default class Subject {
+  constructor(private _name: string) {
+    this.name = _name;
+  };
+
+  get name(): string { return this._name };
+
+  set name(newName:string) {
+    newName.length >= 3 && (this._name = newName);
+  }
+};
